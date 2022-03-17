fbc637fa09ee2d9c458dcba21cffa2b18ccb8352
abrinkmann <alex.brinkmann@informatik.uni-mannheim.de>
2022-03-17 11:59:26 +0100

    Initial commit

---
+++ augment_file.py
@@ -0,0 +1,10 @@
+
+# Create an augmented file with roughly 1.000.000 lines
+with open('X1.csv') as file:
+    lines = file.readlines()
+    writes_per_line = int(1000000 / len(lines[1:]))
+    with open('X1_extended.csv', "w") as output_file:
+        output_file.write(lines[0])
+        for line in lines[1:]:
+            for i in range(0, writes_per_line + 1):
+                output_file.write(line)
---
+++ blocking.py
@@ -0,0 +1,37 @@
+
+import torch
+from datasets import load_dataset
+import time
+
+from transformers import AutoTokenizer, AutoModel
+
+
+
+
+torch.set_grad_enabled(False)
+
+model = AutoModel.from_pretrained("bert-base-uncased")
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+
+ds = load_dataset('csv', data_files='X1_extended.csv', split='train')
+
+#start = time.time()
+#ds_with_embeddings = ds.map(lambda example: {model(**tokenizer(example["title"], return_tensors="pt",
+#                                                                              padding=True, truncation=True, max_length=64))['pooler_output']}, batched=True, batch_size=32)
+
+#ds_with_embeddings.add_faiss_index(column='embeddings')
+
+#end = time.time()
+#print('With batching: ' + str(end - start))
+
+##################################
+
+start = time.time()
+ds_with_embeddings = ds.map(lambda example: {'embeddings': model(**tokenizer(example["title"], return_tensors="pt",
+                                                                              padding=True, truncation=True, max_length=64))['pooler_output'][:, 0].numpy()})
+
+ds_with_embeddings.add_faiss_index(column='embeddings')
+
+end = time.time()
+print('Without batching: ' + str(end - start))
+
