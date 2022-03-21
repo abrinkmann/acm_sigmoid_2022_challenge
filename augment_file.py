4b75e3aa021eaad5c30b446e3c92f2ce0086a93a
abrinkmann <alex.brinkmann@informatik.uni-mannheim.de>
2022-03-21 17:44:59 +0100

    Code for fast CPU indexing

--- augment_file.py
+++ augment_file.py
@@ -2,8 +2,8 @@
 # Create an augmented file with roughly 1.000.000 lines
 with open('X1.csv') as file:
     lines = file.readlines()
-    writes_per_line = int(1000000 / len(lines[1:]))
-    with open('X1_extended.csv', "w") as output_file:
+    writes_per_line = int(100000 / len(lines[1:]))
+    with open('X1_extended_100000.csv', "w") as output_file:
         output_file.write(lines[0])
         for line in lines[1:]:
             for i in range(0, writes_per_line + 1):
--- experiment_cpu_indexing.py
+++ experiment_cpu_indexing.py
@@ -1,104 +1,67 @@
 import itertools
 import logging
 import os
+from datetime import datetime
 
 import click
-import psutil
-import ray
+import numpy
+import pandas as pd
 import torch
-import torch.distributed as dist
-from datasets import load_dataset
-import time
+from datasets import load_dataset, Dataset
 
-from tqdm import tqdm
-from transformers import AutoTokenizer, AutoModel, BertTokenizerFast, BertTokenizer, training_args, pipeline
+from transformers import AutoTokenizer, AutoModel
 
 torch.set_grad_enabled(False)
 
 @click.command()
 @click.option('--file')
-@click.option('--batch_size', type=int, default=16)
-@click.option('--num_proc', type=int, default=2)
-@click.option('--local_rank', type=int, default=0)
-def index_entities(file, batch_size, num_proc, local_rank):
+@click.option('--batch_size', type=int, default=64)
+@click.option('--num_proc', type=int, default=16)
+def index_entities(file, batch_size, num_proc):
 
     logger = logging.getLogger()
-    ds = load_dataset('csv', data_files=file, split='train')
+    # Load data into a pandas data frame
+    df_data = pd.read_csv(file, sep=',', encoding='utf-8')
+    df_data['title'] = df_data['title'].str[:64]
+    df_data = df_data.groupby(by=['title'])['id'].apply(list).reset_index(name='ids')
 
-    model = AutoModel.from_pretrained("bert-base-uncased")
-    tokenizer = BertTokenizerFast.from_pretrained('bert-base-cased')
+    ds = Dataset.from_pandas(df_data)
 
-    #setup(local_rank, 3)
+    tokenizer = AutoTokenizer.from_pretrained("microsoft/xtremedistil-l6-h256-uncased")
+    model = AutoModel.from_pretrained("microsoft/xtremedistil-l6-h256-uncased")
 
-    start = time.time()
+    def encode_and_embed(examples):
+        #tokenized_output = tokenizer(examples['title'], padding="max_length", truncation=True, max_length=64)
+        tokenized_output = tokenizer(examples['title'], padding=True, truncation=True, max_length=64)
+        encoded_output = model(input_ids=torch.tensor(tokenized_output['input_ids']),
+                               attention_mask=torch.tensor(tokenized_output['attention_mask']),
+                               token_type_ids=torch.tensor(tokenized_output['token_type_ids']))
+        return encoded_output['pooler_output'].detach().numpy()
 
-    def tokenize_function(examples):
-        tokenized_output = tokenizer(examples["title"], padding="max_length", truncation=True, max_length=64)
+    start = datetime.now()
+    logger.info('Encode & embed titles!')
 
-        return tokenized_output
+    # Set num proc to number of available CPUs(?)
+    ds_with_embeddings = ds.map(lambda examples: {'embeddings': encode_and_embed(examples)}, batched=True,
+                                batch_size=batch_size, num_proc=num_proc)
 
-    @ray.remote
-    def tokenize_embed(p_model, p_tokenizer, example):
-        tokenized_output = p_tokenizer(example['title'], padding="max_length", truncation=True, max_length=64)
-        encoded_output = p_model(input_ids=torch.tensor([tokenized_output['input_ids']]),
-                               attention_mask=torch.tensor([tokenized_output['attention_mask']]),
-                               token_type_ids=torch.tensor([tokenized_output['token_type_ids']]))
-        return encoded_output['pooler_output'][0].detach().numpy()
-
-    def embedding_function(examples):
-        encoded_output = model(input_ids=torch.tensor(examples['input_ids']),
-                               attention_mask=torch.tensor(examples['attention_mask']),
-                               token_type_ids=torch.tensor(examples['token_type_ids']))
-        output = {'embeddings': encoded_output['pooler_output'].detach().numpy()}
-        return output
-
-    def tokenize_and_embedding_function(examples):
-        return embedding_function(tokenize_function(examples))
-
-    def chunks(lst, n):
-        """Yield successive n-sized chunks from lst."""
-        for i in tqdm(range(0, len(lst), n)):
-            yield lst['title'][i:i + n]
-
-    #ds_with_tokens = ds.map(tokenize_function, batched=True)
-
-    num_cpus = psutil.cpu_count(logical=True)
-    print('Number of available CPUs:', num_cpus)
-
-
-    # Start Ray cluster
-    ray.init(num_cpus=num_cpus, ignore_reinit_error=True)
-
-    model_id = ray.put(model)
-    tokenizer_id = ray.put(tokenizer)
-
-    # Add chunks --> "batches"
-    embeddings = ray.get([tokenize_embed.remote(model_id, tokenizer_id, example) for example in tqdm(ds)])
-    #embeddings = list(itertools.chain(*embeddings))
-    ds_with_embeddings = ds.add_column('embeddings', embeddings)
-        #.map(tokenize_and_embedding_function, batched=True, batch_size=batch_size)
-    #logger.info(f"END PREPROCESSING on RANK {local_rank}")
-
-    #if local_rank == 0:
-    #    logger.info('Loading results from the main process')
-    #    dist.barrier()
-    ray.shutdown()
+    logger.info('Add embeddings to faiss index')
     ds_with_embeddings.add_faiss_index(column='embeddings')
 
-    end = time.time()
-    #cleanup()
-    print('With Ray: ' + str(end - start))
+    end = datetime.now()
+    logger.info('With HF batching: ' + str((end - start).total_seconds() / 60.0))
 
-    ##################################
+    first_embedding = numpy.array([ds_with_embeddings[0]['embeddings']]).astype('float32')
 
-    start = time.time()
-    ds_with_embeddings = ds.map(lambda example: {'embeddings': model(**tokenizer(example["title"], return_tensors="pt",
-                                                                                  padding=True, truncation=True, max_length=64))['pooler_output'][:, 0].numpy()})
+    entity = "Panasonic Latitude 14 B5232 - Solid Duo"
 
-    ds_with_embeddings.add_faiss_index(column='embeddings')
+    entity_embedding = model(**tokenizer(entity, return_tensors="pt"))['pooler_output'].numpy()
 
-    end = time.time()
-    print('Without batching: ' + str(end - start))
+    scores, samples = ds_with_embeddings.get_nearest_examples(
+        "embeddings", first_embedding, k=5
+    )
+
+    print('I am here')
 
 
 if __name__ == '__main__':
