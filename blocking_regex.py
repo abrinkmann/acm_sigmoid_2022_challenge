0f1fc7aeaa4dfbe15b2756b457d31303fe9f6f28
abrinkmann <alex.brinkmann@informatik.uni-mannheim.de>
2022-04-28 12:04:59 +0200

    Long Training +  Block by Regex

--- blocking_regex.py
+++ blocking_regex.py
@@ -11,15 +11,15 @@ import numpy as np
 import torch
 
 from psutil import cpu_count
+from sentence_transformers import SentenceTransformer
 from tqdm import tqdm
 import pandas as pd
 from transformers import AutoTokenizer
 
 #from blocking import block_with_attr
-from blocking_regex import block_regex
+from blocking import block_with_attr
 from model_contrastive import ContrastivePretrainModel
 
-tokenizer = AutoTokenizer.from_pretrained('models/sbert_xtremedistil-l6-h256-uncased-mean-cosine-h32')
 
 def load_normalization():
     """Load Normalization file - Especially for D2"""
@@ -34,7 +34,7 @@ def load_normalization():
 #normalizations = {}
 
 
-def block_neural(X, attr, k_hits, path_to_preprocessed_file, norm, model_type, model_path, seq_length, proj, transitive_closure):  # replace with your logic.
+def block_regex(X, attr, path_to_preprocessed_file, norm, char_length):  # replace with your logic.
     '''
     This function performs blocking using elastic search
     :param X: dataframe
@@ -47,7 +47,7 @@ def block_neural(X, attr, k_hits, path_to_preprocessed_file, norm, model_type, m
 
     worker = cpu_count()
     pool = Pool(worker)
-    X['preprocessed'] = pool.starmap(preprocess_input, zip(list(X[attr].values), repeat(norm), repeat(seq_length)))
+    X['preprocessed'] = pool.starmap(preprocess_input, zip(list(X[attr].values), repeat(norm), repeat(char_length)))
 
     if path_to_preprocessed_file is not None:
         X['tokens'] = pool.map(tokenize_input, tqdm(list(X['preprocessed'].values)))
@@ -76,78 +76,35 @@ def block_neural(X, attr, k_hits, path_to_preprocessed_file, norm, model_type, m
             for k in range(j + 1, len(ids)):
                 candidate_pairs_real_ids.append((ids[j], ids[k]))
 
-    logger.info('Load Models')
+    regex_pattern2id_2 = defaultdict(list)
+    for i in range(len(pattern2id_1)):
+        pattern = list(pattern2id_1.keys())[i]
+        regex_pattern_2 = re.findall("\w+\s\w+\d+", pattern)  # look for patterns like "thinkpad x1"
+        for regex_match in regex_pattern_2:
+            regex_pattern2id_2[regex_match].append(i)
 
-    model = ContrastivePretrainModel(len_tokenizer=len(tokenizer), proj=proj)
-    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
-    model.eval()
 
-    logger.info("Encode & Embed entities...")
-
-    def encode_and_embed(examples):
-        with torch.no_grad():
-            tokenized_input = tokenizer(examples, padding=True, truncation=True, max_length=seq_length,
-                                        return_tensors='pt')
-            encoded_output = model(input_ids=tokenized_input['input_ids'],
-                                   attention_mask=tokenized_input['attention_mask'])
-            return encoded_output[1]
-
-    def chunks(lst, n):
-        """Yield successive n-sized chunks from lst."""
-        for i in tqdm(range(0, len(lst), n)):
-            yield lst[i:i + n]
-
-    embeddings = []
-    for examples in chunks(list(pattern2id_1.keys()), 256):
-        embeddings.append(encode_and_embed(examples))
-    embeddings = np.concatenate(embeddings, axis=0)
-
-    # embeddings = np.concatenate(embeddings)
-    # Make sure that the embeddings are normalized --> cosine similarity
-    logger.info('Initialize faiss index')
-    d = embeddings.shape[1]
-    m = 16
-    nlist = int(4 * math.sqrt(embeddings.shape[0]))
-    quantizer = faiss.IndexFlatIP(d)
-    #faiss_index = faiss.IndexIVFFlat(quantizer, d, nlist)
-    faiss_index = faiss.IndexIVFPQ(quantizer, d, nlist, m, 8)  # 8 specifies that each sub-vector is encoded as 8 bits
-
-    assert not faiss_index.is_trained
-    logger.info('Train Faiss Index')
-    no_training_records = nlist * 40  # Experiment with number of training records
-    if embeddings.shape[0] < no_training_records:
-        faiss_index.train(embeddings)
-    else:
-        train_embeddings = embeddings[np.random.choice(embeddings.shape[0], size=no_training_records, replace=False), :]
-        faiss_index.train(train_embeddings)
-    assert faiss_index.is_trained
-
-    logger.info('Add embeddings to faiss index')
-    faiss_index.add(embeddings)
-
-    logger.info("Search products...")
-    faiss_index.nprobe = 10  # the number of cells (out of nlist) that are visited to perform a search --> INCREASE if possible
-
-    D, I = faiss_index.search(embeddings, k_hits)
-    logger.info('Collect search results')
     pair2sim = defaultdict(float)
-    for index in tqdm(range(len(I))):
-        for distance, top_id in zip(D[index], I[index]):
-            if top_id > -1:
-                if (1 - distance) < 0.1:
-                    break
-                if index == top_id:
-                    continue
-                elif index < top_id:
-                    candidate_group_pair = (index, top_id)
+    for i in range(len(regex_pattern2id_2)):
+        matched_groups = sorted(list(regex_pattern2id_2.values())[i])
+        for j in range(len(matched_groups)):
+            for k in range(j+ 1, len(matched_groups)):
+                group_1 = matched_groups[j]
+                group_2 = matched_groups[k]
+
+                tokens_1 = set(list(pattern2id_1.keys())[group_1].split(' '))
+                tokens_2 = set(list(pattern2id_1.keys())[group_2].split(' '))
+
+                if group_1 < group_2:
+                    candidate_pair = (group_1, group_2)
+                elif group_1 > group_2:
+                    candidate_pair = (group_1, group_2)
                 else:
-                    candidate_group_pair = (top_id, index)
+                    continue
 
-                pair2sim[candidate_group_pair] = max((1 - distance), pair2sim[candidate_group_pair])
+                pair2sim[candidate_pair] = len(tokens_1.intersection(tokens_2)) / max(len(tokens_1), len(tokens_2))
 
-    if transitive_closure:
-        logger.info('Determine transitive pairs')
-        pair2sim = determine_transitive_matches(pair2sim)
+
 
     candidate_group_pairs = [k for k, _ in sorted(pair2sim.items(), key=lambda k_v: k_v[1], reverse=True)]
 
@@ -174,7 +131,7 @@ def block_neural(X, attr, k_hits, path_to_preprocessed_file, norm, model_type, m
     return candidate_pairs_real_ids
 
 
-def preprocess_input(docs, normalizations, seq_length):
+def preprocess_input(docs, normalizations, char_length):
     if len(docs) == 0:
         return ''
     else:
@@ -235,13 +192,7 @@ def preprocess_input(docs, normalizations, seq_length):
         doc = re.sub('\s*$', '', doc)
         doc = re.sub('^\s*', '', doc)
 
-        if len(doc) > 0:
-            tokens = tokenizer.tokenize(doc)
-            pattern = tokenizer.convert_tokens_to_string(tokens[:seq_length])
-        else:
-            pattern = ''
-
-        return pattern
+        return doc[:char_length]
 
 
 def determine_transitive_matches(pairs2sim):
@@ -320,9 +271,7 @@ if __name__ == '__main__':
     normalizations_x_1 = load_normalization()
     #cluster_size_threshold_x1 = None
     transitive_closure_x_1 = False
-    X1_candidate_pairs = block_neural(X_1, ["title"], k_x_1, None, normalizations_x_1, 'supcon',
-                                      'models/supcon/len{}/X1_model_len{}_trans{}_with_computers_longtrain.bin'.format(seq_length_x_1, seq_length_x_1,
-                                                                                              proj_x_1), seq_length_x_1, proj_x_1, transitive_closure_x_1)
+    X1_candidate_pairs = block_regex(X_1, ["title"], None, normalizations_x_1, 120)
     #X1_candidate_pairs = block_with_attr(X_1, "title")
     if len(X1_candidate_pairs) > expected_cand_size_X1:
         X1_candidate_pairs = X1_candidate_pairs[:expected_cand_size_X1]
